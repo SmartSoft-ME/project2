8578d9b7a5945479d5d7daeb60c4d9bceed06cb2
m.jawad@smartsoft-me.com <m.jawad@smartsoft-me.com>
2023-07-01 11:24:55 +0300

    Naming conventions, nullabilty checks

---
+++ Patient.cs
@@ -0,0 +1,20 @@
+﻿namespace project2
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Address { get; set; }
+        public int Age { get; set; }
+
+        public string Injury;
+        public Patient(int id, string name, string? address, int age, string injury)
+        {
+            Injury = injury;
+            Id = id;
+            Name = name;
+            Address = address;
+            Age = age;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,120 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using project2;
 
-namespace project2
+List<Patient> patients = new();
+void CreatePatient()
 {
-    public class Patient
+    Console.WriteLine("Name:");
+    var name = Console.ReadLine();
+    while (string.IsNullOrEmpty(name))
     {
-
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public int Age { get; set; }
-
-        public string Injury;
-        public Patient(int id, string name, string address, int age, string injury)
-        {
-            Injury = injury;
-            Id = id;
-            Name = name;
-            Address = address;
-            Age = age;
-        }
+        Console.WriteLine("Name can't be null or empty!");
+        Console.WriteLine("Name:");
+        name = Console.ReadLine();
     }
-    internal class Program
+    Console.WriteLine("Address:");
+    var address = Console.ReadLine();
+    Console.WriteLine("Age:");
+    var inputAge = Console.ReadLine();
+    while (string.IsNullOrEmpty(inputAge))
     {
-        static List<Patient> lst = new List<Patient>();
-        static void CreatePatient()
-        {
-            Console.WriteLine("Id");
-            int id = int.Parse(Console.ReadLine());
-            Console.WriteLine("Name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Address");
-            string address = Console.ReadLine();
-            Console.WriteLine("age");
-            int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("injury");
-            string injury = Console.ReadLine();
-            Patient pa = new Patient(id, name, address, age, injury);
-            lst.Add(pa);
+        Console.WriteLine("Age can't be null or empty!");
+        Console.WriteLine("Age:");
+        inputAge = Console.ReadLine();
+    }
+    int age = int.Parse(inputAge);
+    Console.WriteLine("Injury");
+    var injury = Console.ReadLine();
+    while (string.IsNullOrEmpty(injury))
+    {
+        Console.WriteLine("Injury can't be null or empty!");
+        Console.WriteLine("Injury:");
+        injury = Console.ReadLine();
+    }
+    int id = patients.Max(p => p.Id) + 1;
+    var patient = new Patient(id, name, address, age, injury);
+    patients.Add(patient);
 
-        }
-        static void RemovePatient()
-        {
-            Console.WriteLine("give the patient id to remove");
-            int id = int.Parse(Console.ReadLine());
-            Patient pa = lst.Find(p => p.Id == id);
-            if (pa != null)
-            {
-                lst.Remove(pa);
-                Console.WriteLine("the patient is removed");
-            }
-            else
-                Console.WriteLine("the patient didnt exist");
-        }
-        static void UpdatePatient()
-        {
-            Console.WriteLine("give the patient id to update");
-            int id = int.Parse(Console.ReadLine());
-            Patient pa = lst.Find(p => p.Id == id);
-            if (pa != null)
-            {
-                Console.WriteLine("Enter new patient details:");
-                Console.Write("Name: ");
-                pa.Name = Console.ReadLine();
-                Console.Write("Age: ");
-                pa.Age = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Address: ");
-                pa.Address = Console.ReadLine();
-                Console.WriteLine("Injury");
-                pa.Injury = Console.ReadLine();
-            }
-            else
-                Console.WriteLine("patient unavailable");
-        }
-        static void Display()
-        {
-            foreach (var p in lst)
-            {
-                Console.WriteLine("ID: " + p.Id);
-                Console.WriteLine("Name: " + p.Name);
-                Console.WriteLine("Address: " + p.Address);
-                Console.WriteLine("Age: " + p.Age);
-                Console.WriteLine("Injury: " + p.Injury);
-            }
-        }
+}
+void RemovePatient()
+{
+    Console.WriteLine("Give the patient id to remove.");
+    var inputId = Console.ReadLine();
+    while (string.IsNullOrEmpty(inputId))
+    {
+        Console.WriteLine("Id can't be null or empty!");
+        Console.WriteLine("Give the patient id to remove.");
+        inputId = Console.ReadLine();
+    }
+    int id = int.Parse(inputId);
+    var patient = patients.Find(p => p.Id == id);
+    if (patient != null)
+    {
+        patients.Remove(patient);
+        Console.WriteLine($"{patient.Name} is removed");
+    }
+    else
+        Console.WriteLine("The patient doesn't exist");
+}
+void UpdatePatient()
+{
+    Console.WriteLine("give the patient id to update");
+    int id = int.Parse(Console.ReadLine());
+    Patient pa = patients.Find(p => p.Id == id);
+    if (pa != null)
+    {
+        Console.WriteLine("Enter new patient details:");
+        Console.Write("Name: ");
+        pa.Name = Console.ReadLine();
+        Console.Write("Age: ");
+        pa.Age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Address: ");
+        pa.Address = Console.ReadLine();
+        Console.WriteLine("Injury");
+        pa.Injury = Console.ReadLine();
+    }
+    else
+        Console.WriteLine("patient unavailable");
+}
+void Display()
+{
+    foreach (var p in patients)
+    {
+        Console.WriteLine("ID: " + p.Id);
+        Console.WriteLine("Name: " + p.Name);
+        Console.WriteLine("Address: " + p.Address);
+        Console.WriteLine("Age: " + p.Age);
+        Console.WriteLine("Injury: " + p.Injury);
+    }
+}
 
-        static void Main()
-        {
-            while (true)
-            {
-                Console.WriteLine("1-create a pateint");
-                Console.WriteLine("2-remove a patient");
-                Console.WriteLine("3-update a patient");
-                Console.WriteLine("4-display the patients");
+while (true)
+{
+    Console.WriteLine("1-create a patient");
+    Console.WriteLine("2-remove a patient");
+    Console.WriteLine("3-update a patient");
+    Console.WriteLine("4-display the patients");
 
-                int choix = Convert.ToInt32(Console.ReadLine());
-                switch (choix)
-                {
-                    case 1:
-                        CreatePatient();
-                        break;
-                    case 2:
-                        RemovePatient();
-                        break;
-                    case 3:
-                        UpdatePatient();
-                        break;
-                    case 4:
-                        Display();
-                        break;
-                    default:
-                        Console.WriteLine("invalid,please choose another option:");
-                        break;
-                }
-            }
-        }
-
+    int choice = Convert.ToInt32(Console.ReadLine());
+    switch (choice)
+    {
+        case 1:
+            CreatePatient();
+            break;
+        case 2:
+            RemovePatient();
+            break;
+        case 3:
+            UpdatePatient();
+            break;
+        case 4:
+            Display();
+            break;
+        default:
+            Console.WriteLine("invalid,please choose another option:");
+            break;
     }
 }
